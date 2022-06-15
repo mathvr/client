bad68ee305c5fd575c57705f3ea29511af5cc69c
Math <math.vr@protonmail.com>
2022-06-15 00:31:11 -0400

    commit

---
+++ src/features/ingredient/IngredientPage.tsx
@@ -0,0 +1,9 @@
+import {Typography} from "@mui/material";
+
+export default function IngredientPage (){
+    return(
+        <Typography variant='h2'>
+            Ingredients
+        </Typography>
+    )
+}
